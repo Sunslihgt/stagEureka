7126963c45e8ef09389876dfb3484b8eb902a5ca
samsa <samsam.deschamps@gmail.com>
2024-04-04 23:44:23 +0200

    Ajout adresse service worker prod/test

--- www/pwa/execution_service_worker.js
+++ www/pwa/execution_service_worker.js
@@ -4,9 +4,12 @@
 // }
 
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register("http://localhost/stageureka/www/pwa/service_worker.js").then(function(registration) {
+    // TODO: Mettre à jour l'URL du service worker en fonction de l'environnement
+    // const url = "https://stageureka.alwaysdata.net/pwa/service_worker.js";  // Production
+    const url = "http://localhost/stageureka/www/pwa/service_worker.js";  // Test en local
+    navigator.serviceWorker.register(url).then(function (registration) {
       console.log("Service Worker en fonctionnement :", registration.scope);
-    }).catch(function(err) {
+    }).catch(function (err) {
       console.log("Service Worker en échec :", err);
     });
   }
